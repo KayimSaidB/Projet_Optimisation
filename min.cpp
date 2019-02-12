44f173e3a0c9b7772ca1ca2284d014a671ee5f6a
Geng <geng2608@hotmail.com>
2019-02-12 12:07:34 +0100

    select index

--- min.cpp
+++ min.cpp
@@ -7,9 +7,34 @@ Min::Min(int m, int n):nb_cont(m), nb_var(n), fonction_cout(n,0.0), contrainte_g
 {
 	for(int i = 0; i < m ; ++i)
 	{
-		contrainte_gauche[i].resize(n,0);
+		contrainte_gauche[i].resize(n);
 	}
 }
 
 Min::~Min()
 {}
+
+int Min::indice_max()
+{
+	return std::distance(fonction_cout.begin(), std::max_element(fonction_cout.begin(), fonction_cout.end()));
+}
+
+int Min::indice_min()
+{
+	return std::distance(fonction_cout.begin(), std::min_element(fonction_cout.begin(), fonction_cout.end()));
+}
+
+bool Min::isSatisfy()
+{
+	for(int i = 0; i<nb_cont; ++i)
+	{
+		double sum = 0;
+		for(int j = 0; j<nb_var; ++j)
+		{
+			sum += contrainte_gauche[i][j].first;
+		}
+		if(sum > contrainte_droite[i]) return false;
+	}
+
+	return true;
+}
--- min.hh
+++ min.hh
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <vector>
+#include <utility>
+#include "algorithm"
+#include <math.h>
 
 class Min
 {
@@ -9,11 +12,14 @@ public:
 	Min(int m, int n); // m nombre de contraintes, n nombre de variables
 	~Min();
 
+	int indice_max(); // retourne l'indice de la variable à minimiser
+	int indice_min();
+	bool isSatisfy(); // regarde si tous les contraintes sont satisfaites
 private:
 	int nb_cont; // nombre de contrainte
 	int nb_var; // nombre de variable
 
 	std::vector<double> fonction_cout;
-	std::vector<std::vector<double>> contrainte_gauche; // partie gauche de la contrainte
+	std::vector< std::vector< std::pair<double,double> > > contrainte_gauche; // partie gauche de la contrainte
 	std::vector<double> contrainte_droite; // partie droite de la contrainte
 };
